a53bb170dc56ced419186b8b1e536653dbd8fca2
Kennedy Aquino <kennedy.eletronica.94@gmail.com>
2020-08-12 20:45:42 -0300

    Service layer, component registration - part 1

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String fone;
 	private String password;
+	
+	// MAPEADO COM O PRODUTO CLIENT NA CLASSE ORDER
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -71,6 +80,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
